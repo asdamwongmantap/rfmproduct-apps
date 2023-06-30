a2091862f392094379639a761d83402f6bd075aa
asdamwongmantap <asdamgrimson@gmail.com>
2023-06-30 09:53:05 +0700

    change title and add requirement

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from apps import dashboard,analyze,cluster # import your app modules here
 import pandas as pd
 from streamlit_option_menu import option_menu
 
+st.set_page_config(page_title="RFMProduct-Apps", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 with st.sidebar:
     selected = option_menu("Main Menu", ["Dashboard","Analisa Produk", 'Klaster Produk'], 
         icons=['house','basket', 'pie-chart'], menu_icon="cast", default_index=1)
