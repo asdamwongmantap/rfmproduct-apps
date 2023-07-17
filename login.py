4610fc7213ac0d2d7b635f0d8c2cab68b2dcd948
asdamwongmantap <asdamgrimson@gmail.com>
2023-07-17 18:18:11 +0700

    add dev login

---
+++ login.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    if username == 'operation':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 1')
+    elif username == 'finance':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 2')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
