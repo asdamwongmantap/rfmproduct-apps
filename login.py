7a50f82d0a15209b999984f0e40e4637896ee126
asdamwongmantap <asdamgrimson@gmail.com>
2023-07-17 20:55:17 +0700

    set page

--- login.py
+++ login.py
@@ -8,6 +8,8 @@ from streamlit_option_menu import option_menu
 
 hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 
+st.set_page_config(page_title="RFMProduct-Apps", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
@@ -22,8 +24,7 @@ authenticator = stauth.Authenticate(
 name, authentication_status, username = authenticator.login("Login", "main")
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    st.set_page_config(page_title="RFMProduct-Apps", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
-
+    
     with st.sidebar:
         selected = option_menu("Main Menu", ["Dashboard","Analisa Produk", 'Klaster Produk'], 
             icons=['house','basket', 'pie-chart'], menu_icon="cast", default_index=0)
