49f03e0770077981e3529eecb52b8b1d6fc346cd
asdamwongmantap <asdamgrimson@gmail.com>
2023-07-20 15:37:34 +0700

    revert text dashboard

--- apps/dashboard.py
+++ apps/dashboard.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 
 def app():
-    # st.title('Welcome To RFM-Product Dashboard')
+    st.markdown("---")
+    st.title('Welcome To RFM-Product Dashboard')
 
-    # st.write(" RFM-Product Dashboard merupakan tampilan visual yang dapat digunakan untuk mempresentasikan informasi mengenai produk secara ringkas dan terstruktur. Dashboard ini dapat digunakan untuk memantau produk, menganalisis tren penjualan produk, dan dapat digunakan sebagai bahan untuk pengambilan keputusan .")
+    st.write(" RFM-Product Dashboard merupakan tampilan visual yang dapat digunakan untuk mempresentasikan informasi mengenai produk secara ringkas dan terstruktur. Dashboard ini dapat digunakan untuk memantau produk, menganalisis tren penjualan produk, dan dapat digunakan sebagai bahan untuk pengambilan keputusan .")
     
-    # st.write('Silahkan Pilih Menu Sidebar untuk mulai melakukan analisa')
-    st.write('')
+    st.write('Silahkan Upload File dan Pilih Menu Sidebar untuk mulai melakukan analisa')
+    # st.write('')
+    st.markdown("---")
